5c3c876d42fe356266fc4ebb38c656e4fe2d8d89
Monica <monica.garcia.miranda@gmail.com>
2023-04-15 03:54:12 -0400

    orden

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 function muestraLaMagia() {
     var resultado = document.getElementById("resultado");
     var a = parseInt(document.getElementById("a").value);
-    var b = parseInt(document.getElementById("b").value);
-    var area = a * b;
+    // var b = parseInt(document.getElementById("b").value);
+    var areaCua = a * a;
+    var areaTri = a * a * 0.25 * 1.7320508 ;
+    var areaCir = a * a * 3.14159;
 
-    resultado.innerHTML = "El resultado es: " + area;
+    var periCua = a * 4;
+    var periTri = a * 3 ;
+    var periCir = 2 * a * 3.14159;
+
+    resultado.innerHTML = "El resultado es: " + areaCua;
+    resultado.innerHTML = "El resultado es: " + areaTri;
+    resultado.innerHTML = "El resultado es: " + areaCir;
+    resultado.innerHTML = "El resultado es: " + periCua;
+    resultado.innerHTML = "El resultado es: " + periTri;
+    resultado.innerHTML = "El resultado es: " + periCir;
 
 var img = document.getElementById("magicGif");
 img.style.display = "inline";
@@ -12,7 +23,6 @@ img.style.display = "inline";
 
 function limpiarCampos() {
     document.getElementById("a").value="";
-    document.getElementById("b").value="";
     document.getElementById("resultado").innerHTML="";
     magicGif.style.display="none";
 }
